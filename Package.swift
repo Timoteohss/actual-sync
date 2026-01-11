5a25f2d9f03a736da43811965cada84b0f8dfa04
Tim <timoteohss@gmail.com>
2026-01-11 07:34:37 -0300

    Update XCFramework checksum for v0.1.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "ActualSync",
             url: "https://github.com/Timoteohss/actual-sync/releases/download/v0.1.0/ActualSync.xcframework.zip",
-            checksum: "00798de8a4a5f8569dbb99482a88ac264fe1c7d669a90b8bf3790b1d06d0369c"
+            checksum: "815e27868f05e28a4338ab914dd7eb40a29e0f63159598c57d5c2f9600f187d8"
         ),
     ]
 )
